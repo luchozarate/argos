cb6cd5fbecaaa62e2e7a69de6b45f5fa7ea4062e
Lucho Zarate <luchozarateh@gmail.com>
2026-07-09 00:03:16 -0300

    Add JWT support

---
+++ backend/app/security/jwt.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+
+from jose import jwt
+
+from app.core.config import settings
+
+
+def create_access_token(
+    data: dict,
+) -> str:
+
+    to_encode = data.copy()
+
+    expire = datetime.now(
+        timezone.utc
+    ) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update(
+        {"exp": expire}
+    )
+
+    return jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM,
+    )
